dcb16aec13c1b74809d97d68b10525b0bea5e544
j_n_t_ <joaonsstorres@hotmail.com>
2019-06-09 19:07:32 +0100

    train seq2seq model

--- chatbot.py
+++ chatbot.py
@@ -165,7 +165,7 @@ for length in range(1, 25+1): # maximum length of questions is 25 words
 def model_inputs():
     inputs = tf.placeholder(tf.int32, [None, None], name = 'input')
     targets = tf.placeholder(tf.int32, [None, None], name = 'target')
-    lr = tf.placeholder(tf.float32, name = 'learning rate')
+    lr = tf.placeholder(tf.float32, name = 'learning_rate')
     keep_prob = tf.placeholder(tf.float32, name = 'keep_prob') # parameter that controls the dropout rate
     return inputs, targets, lr, keep_prob
 
@@ -295,10 +295,63 @@ def seq2seq_model(inputs, targets, keep_prob, batch_size, sequence_length,
                                                          keep_prob,
                                                          batch_size)
     return training_predictions, test_predictions
+
+
+# TRAINING THE SEQ2SEQ MODEL
+
+# Setting the hyperparameters
+epochs = 100
+batch_size = 64
+rnn_size = 512
+num_layers = 3
+encoding_embedding_size = 512
+decoding_embedding_size = 512
+learning_rate = 0.01
+learning_rate_decay = 0.9
+min_learning_rate = 0.0001
+keep_probability = 0.5
  
-
-
-
+# Defining a session
+tf.reset_default_graph()
+session = tf.InteractiveSession()
+ 
+# Loading the model inputs
+inputs, targets, lr, keep_prob = model_inputs()
+ 
+# Setting the sequence length
+sequence_length = tf.placeholder_with_default(25, None, name = 'sequence_length')
+ 
+# Getting the shape of the inputs tensor
+input_shape = tf.shape(inputs)
+ 
+# Getting the training and test predictions
+training_predictions, test_predictions = seq2seq_model(tf.reverse(inputs, [-1]),
+                                                       targets,
+                                                       keep_prob,
+                                                       batch_size,
+                                                       sequence_length,
+                                                       len(answers_words_to_int),
+                                                       len(questions_words_to_int),
+                                                       encoding_embedding_size,
+                                                       decoding_embedding_size,
+                                                       rnn_size,
+                                                       num_layers,
+                                                       questions_words_to_int)
+ 
+# Setting up the Loss Error, the Optimizer and Gradient Clipping
+with tf.name_scope("optimization"):
+    loss_error = tf.contrib.seq2seq.sequence_loss(training_predictions,
+                                                  targets,
+                                                  tf.ones([input_shape[0], sequence_length]))
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    gradients = optimizer.compute_gradients(loss_error)
+    clipped_gradients = [(tf.clip_by_value(grad_tensor, -5., 5.), grad_variable) for grad_tensor, grad_variable in gradients if grad_tensor is not None]
+    optimizer_gradient_clipping = optimizer.apply_gradients(clipped_gradients)
+ 
+# Padding the sequences with the <PAD> token
+def apply_padding(batch_of_sequences, word_to_int):
+    max_sequence_length = max([len(sequence) for sequence in batch_of_sequences])
+    return [sequence + [word_to_int['<PAD>']] * (max_sequence_length - len(sequence)) for sequence in batch_of_sequences]
 
 
 
